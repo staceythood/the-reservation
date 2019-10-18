463ff732818703000d12b62d4af99669d423edcd
staceythood <staceyterrell@hotmail.com>
2019-10-17 20:30:16 -0500

    Justin said so

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import re
 from geocoder import Geocoder
